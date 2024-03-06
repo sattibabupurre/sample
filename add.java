504bbc7a8db9294c0338dc624dd86adbbe83aea2
sattibabupurre <159251493+sattibabupurre@users.noreply.github.com>
2024-03-06 16:14:23 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,13 +1,9 @@
 Iclass add {
-
-  public static void main(String[] args) {
-    
-    int first = 10;
+public static void main(String[] args) {
+   int first = 10;
     int second = 20;
-
-    // add two numbers
+ // add two numbers
     int sum = first + second;
     System.out.println(first + " + " + second + " = "  + sum);
   }
 }
-
